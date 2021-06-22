75c060503cfed3787902c774b14406dfdf70054b
Robert B <robertb@lifesciencepublishers.com>
2021-06-22 12:14:18 -0600

    upload files

--- Array_test/Find_Median_Sorted_Arrays/js/index.js
+++ Array_test/Find_Median_Sorted_Arrays/js/index.js
@@ -1,4 +1,4 @@
-const nums1 = [3,4,5], nums2 = [1,2];
+const nums1 = [3,4,5], nums2 = [1,2], testArray = [9, 10, 12, 13, 13, 13, 15, 15, 16, 16, 18, 22, 23, 24, 24, 25];
 
 // Created My own Merge Sort Algorithm 97% in LeetCode
 function findMedianSortedArrays(nums1, nums2) {
@@ -47,5 +47,65 @@ function findMedianSortedArrays2(nums1, nums2) {
         const right = results[(results.length-1) - results.length/2];
         let median = (left + right)/2;
         return median;
+    }
+}
+
+/** Other Statistics Functions   **/
+
+function findMean(nums) {
+    let sum = 0;
+    nums.forEach(toSum => {
+        sum += toSum
+    });
+    return sum/nums.length;
+}
+
+function findMedian(nums) {
+    nums.sort((a,b) => a-b);
+    if(nums.length % 2 !== 0){
+        return nums[Math.floor(nums.length / 2)];
+    } else {
+        const left = nums[nums.length / 2];
+        const right = nums[(nums.length-1) - nums.length/2];
+        let median = (left + right)/2;
+        return median;
+    }
+}
+
+function findMode(nums) {
+    nums.sort((a,b) => a-b);
+    let bestStreak = 1, bestElem = nums[0], currentStreak = 1, currentElem = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if(nums[i-1] !== nums[i]){
+            if(currentStreak > bestStreak){
+                bestStreak = currentStreak;
+                bestElem = currentElem;
+            }
+            currentStreak = 0;
+            currentElem = nums[i];
+        }
+        currentStreak++;
+    }
+    return currentStreak > bestStreak ? currentElem : bestElem;
+}
+
+function findRange(nums) {
+    nums.sort((a,b) => a-b);
+    const left = nums[0];
+    const right = nums[nums.length-1];
+    return right - left;
+}
+
+function findIQR(nums) {
+    nums.sort((a,b) => a-b);
+    const half_length = Math.ceil(nums.length / 2);    
+    const leftSide = nums.splice(0,half_length);
+    const rightSide = nums.splice(half_length.length, half_length);
+    const q1 = findMedian(leftSide);
+    const q2 = findMedian(rightSide);
+    if(q1 < q2){
+        return q2 - q1;
+    } else {
+        return q1 - q2;
     }
 }
