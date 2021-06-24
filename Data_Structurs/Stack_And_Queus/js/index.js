5cd8f5881225f9c257e1772eb19029c4bcd642df
Robert B <robertb@lifesciencepublishers.com>
2021-06-24 16:53:13 -0600

    upload files

---
+++ Data_Structurs/Stack_And_Queus/js/index.js
@@ -0,0 +1,65 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0
+    }
+    push(val){
+        let newNode = new Node(val);
+        if(this.size === 0){
+            this.first = newNode;
+            this.last = this.first;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.last.next = temp;
+        }
+        return ++this.size;
+    }
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val){
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+    dequeue(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
