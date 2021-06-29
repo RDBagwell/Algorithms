31a5a463a32afa96dea5ac13baa961ab3a0dd9bb
Robert B <robertb@lifesciencepublishers.com>
2021-06-29 16:43:13 -0600

    upload files

--- Data_Structurs/Binary_Search_Trees/js/index.js
+++ Data_Structurs/Binary_Search_Trees/js/index.js
@@ -49,14 +49,62 @@ class BinarySearchTree{
         if(!found) return undefined;
         return current;
     }
+
+    // Breadth First Search
+    BFS(){
+        let node = this.root, data = [], queue = [];
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
+    // Depth First Search
+    DFSPreOrder(){
+        let data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right); 
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSPostOrder(){
+        let data = [];
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+    
+    DFSInOrder(){
+        let data = [];
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);           
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 var tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
-console.log(tree.find(7));
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.DFSPreOrder();
+tree.DFSPostOrder();
+console.log(tree.DFSInOrder());
