177ca02d478a79c20806a591d34d24931a65eb2b
Robert B <robertb@lifesciencepublishers.com>
2021-08-05 14:18:31 -0600

    Add cipher

--- Array_test/Cipher/js/index.js
+++ Array_test/Cipher/js/index.js
@@ -1,11 +1,12 @@
 const sentence = 'The quick brown fox jumps over the lazy dog. It barked.';
 
-const encrypted = cipher(sentence, 13)
+const encrypted = cipher(sentence, 1)
 
 function cipher(text, n) {
     const chars = text.split('')
     const shiftarry = [];
     const fixedArr = [];
+
     chars.forEach(char=>{
         let ASCII =  char.charCodeAt();
         if(ASCII >= 65 && ASCII <= 90){
@@ -25,10 +26,41 @@ function cipher(text, n) {
         }
         
     });
+
     shiftarry.forEach(e=>{
         fixedArr.push(String.fromCharCode(e))
     })
+
     return fixedArr.join('')
 }
 
-console.log(encrypted)
+function deCipher(text, n) {
+    const chars = text.split('')
+    const shiftarry = [];
+    const fixedArr = [];
+
+    chars.forEach(char=>{
+        let ASCII =  char.charCodeAt();
+        if(ASCII >= 65 && ASCII <= 90){
+            let uppStr = ASCII - n;
+            if(uppStr < 65){
+                uppStr = 90 - (64 - uppStr)
+            }
+            shiftarry.push(uppStr); 
+        } else if(ASCII >= 97 && ASCII<= 122){
+            let loStr = ASCII - n;
+            if(loStr < 97){
+                loStr = 122 - (96 - loStr)
+            }
+            shiftarry.push(loStr); 
+        } else{
+            shiftarry.push(ASCII);
+        }
+    });
+
+    shiftarry.forEach(e=>{
+        fixedArr.push(String.fromCharCode(e))
+    })
+
+    return fixedArr.join('')
+}
